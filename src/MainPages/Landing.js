570a21faa2e28b3a2554f9a9211bca742827fdfc
Christine Gomez <christine.gomez1226@gmail.com>
2019-01-28 23:10:13 -0500

    added dropdown menu and edited background

--- src/MainPages/Landing.js
+++ src/MainPages/Landing.js
@@ -8,10 +8,15 @@ export default class LandingPage extends React.Component{
       <Nav />
         <div className="landing-page">
           <div className="columns1">
-            LandingPage
+            <h2>Christine Gomez</h2>
+            <p>missing image</p>
           </div>
           <div className="columns2">
-            LandingPage
+            <p>Full Stack Developer passionate about debugging code.</p>
+            <p> With a background in customer service. Very comfortable when</p>
+            <p> working with other people to complete a greater task. Now in the</p>
+            <p> web development industry, very excited to push limits and create</p>
+            <p> bigger projects in a dev team.</p>
           </div>
         </div>
       </div>
--- src/Nav.js
+++ src/Nav.js
@@ -2,25 +2,33 @@ import React from 'react'
 
 export default class Nav extends React.Component{
 
+
   render(){
     return(
       <div>
         <div className="ui secondary pointing menu">
-          <button className="item collapsible">
-            Menu
-          </button>
+          <div>
+            <div className="ui simple dropdown item">
+              Menu
+              <div className="menu">
+                <div className="item">Bio</div>
+                <div className="item">Projects</div>
+                <div className="item">Contact</div>
+              </div>
+            </div>
+          </div>
         <div className="right menu">
           <a href="https://www.linkedin.com/in/christine-gomez/" className="ui item">
-            <i class="linkedin icon"></i>
+            <i className="linkedin icon"></i>
           </a>
           <a href="https://github.com/christine1226" className="ui item">
-            <i class="github icon"></i>
+            <i className="github icon"></i>
           </a>
           <a href="https://www.instagram.com/christineeee_g/?hl=en" className="ui item">
-            <i class="instagram icon"></i>
+            <i className="instagram icon"></i>
           </a>
           <a href="https://medium.com/@christine.gomez1226" className="ui item">
-            <i class="medium icon"></i>
+            <i className="medium icon"></i>
           </a>
         </div>
         </div>
