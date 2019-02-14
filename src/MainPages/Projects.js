3da74b70ed1b166460ec64df1f06e710c175413a
Christine Gomez <christine.gomez1226@gmail.com>
2019-02-14 17:52:06 -0500

    styled project component

--- src/MainPages/Projects.js
+++ src/MainPages/Projects.js
@@ -10,15 +10,19 @@ class Projects extends React.Component{
           <h1>Projects</h1>
           <div className="proj1">
             <div className="blink"></div>
+            <p>JavaScript & Ruby on Rails</p>
           </div>
           <div className="proj1">
             <div className="sea"></div>
+            <p>React/Redux & Ruby on Rails</p>
           </div>
           <div className="proj1">
             <div className="lit"></div>
+            <p>Ruby & Ruby on Rails</p>
           </div>
           <div className="proj1">
             <div className="boozer"></div>
+            <p>Ruby on Rails & React</p>
           </div>
         </div>
       </div>
